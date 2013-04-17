881633059e313eae49bcc22ee7d0bdb7364b05fa
FisHinLab <fishinlab@sina.com>
2013-04-17 22:08:16 +0800

    FisHinLab20130417

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import Tkinter
 import os
+import re
 import sys
 from Tkinter import *
 from xml_merger import Mario
@@ -12,19 +13,71 @@ default_width = 640
 default_bar_height = 480
 default_bar_width = 120
 
+#fucntion: upload xml files and merge them
+#parameters: @merger => <string> xml file path
+#            @mergee => <string> xml file path
+#            @result => <string> xml result path
 def do_merge():
-#this button function is to read parameters from setting.conf file, and do merge job
+    fd_setting = file(os.getcwd() + os.sep + "setting.conf", "r")
+    fd_config = fd_setting.read()
+    fd_setting.close()
+    
+    if(fd_config):
+        ex_pathes = ["MERGER_PATH:.*", "MERGEE_PATH:.*", "RESULT_PATH:.*"]
+        para_pathes = {}
+        try:
+            for i in range(3):
+                ex_tmp = re.compile(ex_pathes[i])
+                para_pathes[ex_pathes[i]] = (ex_tmp.findall(fd_config)[0]).strip(ex_pathes[i])
+            merger, mergee, result = para_pathes.values()
+#DEBUG: 
+#            print >> sys.stdout, merger
+#            print >> sys.stdout, mergee
+#            print >> sys.stdout, result 
+            m = Mario()
+            m.load_merger_file(merger)
+            m.load_mergee_file(mergee)
+            succ_scp_name = m.get_succ_script_node(merger)
+            fail_scp_name = m.get_fail_script_node(mergee)
+
+            m.merge_xml(m.merger_tree, m.mergee_tree, succ_scp_name, fail_scp_name)
+            m.generate_xml_tree(m.result_tree, result)
+        except:
+            print >> sys.stdout, "define parameters firstly"
+            return 
     print >> sys.stdout, "clicked merge button"
 
 def do_deploy():
 #this button function is to deploy the html report with outlook to the boss
 #because of the environment, this fucntion should be done with exists jar package
+    fd_setting = file(os.getcwd() + os.sep + "setting.conf", "r")
+    fd_config = fd_setting.read()
+    fd_setting.close()
+
+    f_xmls_path = "" 
+    if(fd_config):
+        try:
+            ex_sum_models = re.compile("REPORTS_PATH.*")
+            f_xmls_path = ex_sum_models.findall(fd_config)[0] 
+        except:
+            print >> sys.stdout, "define parameters firstly"
+
+    report_names = []
+    for f in os.listdir(f_xmls_path):
+        if "." in f and "xml" == (f.split(".")[1]).lower():
+            report_names.append(f)
+    
+    reporter = html_reporter(reports_path = f_xmls_path, report_names = report_names)
+    for f in report_names:
+		reporter.parse_xml(f_xmls_path + f)
+
+    reporter.generate_html_report()
     print >> sys.stdout, "clicked deploy button"
 
 def do_setting():
 #this button function is to set parameters such like xml, html report path, email client and so on
 #using JSON file style to store users'  report parameters
-    fd_setting = file(getcwd() + "setting.conf", "w")
+    fd_setting = file(os.getcwd() + os.sep + "setting.conf", "w")
     print >> fd_setting, "here is a setting test"
 
 def run():
